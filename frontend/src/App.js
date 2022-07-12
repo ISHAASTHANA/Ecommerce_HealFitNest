4b833c99161e4c4b9ce64e66f69f1db03d7ab449
APOORVANAGAR-2002 <apoorvanagar2002@gmail.com>
2022-07-13 03:51:18 +0530

    added the react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 // import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
     </div>
   );
 }
