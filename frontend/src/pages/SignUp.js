2aa1f832896b505ce6d7c7d6146e1245f9c7deaf
Ananya Priya <ananyapriya1003@gmail.com>
2022-08-25 23:15:11 +0530

    Added Signup

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -39,7 +39,7 @@ const Signup = () => {
     console.log(user);
     axios.post(`${baseUrl}/v2/registerUser`, user).then((response) => {
       console.log(response);
-      if (response.data === "User Added Successfully") {
+      if (response.data === "User added successfully") {
         alert('New user added!');
         navigate('/login');
       }
